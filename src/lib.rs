0b442fbae5900bb2f9001cf0796372787a262bc0
Vyr Cossont <VyrCossont@users.noreply.github.com>
2022-09-02 14:14:08 -0700

    Update SFX playback to current version of pico8_rom_tool

--- src/lib.rs
+++ src/lib.rs
@@ -7,32 +7,44 @@ mod wasm4;
 use font::*;
 use wasm4::*;
 
-struct Wasm4Tone {
+struct Tone {
     frequency: u32,
     duration: u32,
     volume: u32,
     flags: u32,
 }
 
-#[derive(Default)]
-struct SfxPlayback<'a> {
+struct Sfx<'a> {
     frames_per_tone: u8,
+    loop_restart: Option<usize>,
+    tones: &'a [Tone],
+}
+
+struct SfxPlayback<'a> {
     frame_counter: u8,
-    tone_counter: u8,
-    tones: &'a [Wasm4Tone],
+    tone_index: usize,
+    sfx: &'a Sfx<'a>,
 }
 
 impl SfxPlayback<'_> {
     fn playing(&self) -> bool {
-        (self.tone_counter as usize) < self.tones.len()
+        self.tone_index < self.sfx.tones.len()
     }
 
-    fn update(&mut self) {
+    /// Returns whether it will continue to play.
+    /// Always returns true for looped SFX.
+    fn update(&mut self) -> bool {
         if !self.playing() {
-            return;
+            if let Some(loop_restart) = self.sfx.loop_restart {
+                self.frame_counter = 0;
+                self.tone_index = loop_restart;
+            } else {
+                return false;
+            }
         }
+
         if self.frame_counter == 0 {
-            let sfx_tone = &self.tones[self.tone_counter as usize];
+            let sfx_tone = &self.sfx.tones[self.tone_index];
             if sfx_tone.volume > 0 {
                 tone(
                     sfx_tone.frequency,
@@ -43,58 +55,119 @@ impl SfxPlayback<'_> {
                 trace("tone");
             }
         }
+
         self.frame_counter += 1;
-        if self.frame_counter == self.frames_per_tone {
+        if self.frame_counter == self.sfx.frames_per_tone {
             self.frame_counter = 0;
-            self.tone_counter += 1;
+            self.tone_index += 1;
         }
+
+        return true;
     }
 }
 
-const C_TEST: [Wasm4Tone; 7] = [
-    Wasm4Tone {
-        frequency: 130,
-        duration: 8,
-        volume: 71,
-        flags: 2,
-    },
-    Wasm4Tone {
-        frequency: 261,
-        duration: 8,
-        volume: 71,
-        flags: 2,
-    },
-    Wasm4Tone {
-        frequency: 523,
-        duration: 8,
-        volume: 71,
-        flags: 2,
-    },
-    Wasm4Tone {
-        frequency: 1046,
-        duration: 8,
-        volume: 71,
-        flags: 2,
-    },
-    Wasm4Tone {
-        frequency: 2093,
-        duration: 8,
-        volume: 71,
-        flags: 2,
-    },
-    Wasm4Tone {
-        frequency: 65,
-        duration: 8,
-        volume: 0,
-        flags: 2,
-    },
-    Wasm4Tone {
-        frequency: 440,
-        duration: 8,
-        volume: 71,
-        flags: 2,
-    },
-];
+const SFX_DATA: &[Sfx] = &[Sfx {
+    frames_per_tone: 8,
+    loop_restart: Some(0),
+    tones: &[
+        Tone {
+            frequency: 130,
+            duration: 8,
+            volume: 71,
+            flags: 2,
+        },
+        Tone {
+            frequency: 261,
+            duration: 8,
+            volume: 71,
+            flags: 2,
+        },
+        Tone {
+            frequency: 523,
+            duration: 8,
+            volume: 71,
+            flags: 2,
+        },
+        Tone {
+            frequency: 1046,
+            duration: 8,
+            volume: 71,
+            flags: 2,
+        },
+        Tone {
+            frequency: 2093,
+            duration: 8,
+            volume: 71,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 440,
+            duration: 8,
+            volume: 71,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+        Tone {
+            frequency: 65,
+            duration: 8,
+            volume: 0,
+            flags: 2,
+        },
+    ],
+}];
 
 const SFX0: *mut SfxPlayback = 0x19a0 as *mut SfxPlayback;
 
@@ -102,10 +175,9 @@ const SFX0: *mut SfxPlayback = 0x19a0 as *mut SfxPlayback;
 fn start() {
     unsafe {
         *SFX0 = SfxPlayback {
-            frames_per_tone: 8,
             frame_counter: 0,
-            tone_counter: 0,
-            tones: &C_TEST,
+            tone_index: 0,
+            sfx: &SFX_DATA[0],
         }
     }
 }
