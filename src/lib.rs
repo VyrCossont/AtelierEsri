5be93720805668ecfcf145998adc4bbc6a340a88
Vyr Cossont <VyrCossont@users.noreply.github.com>
2022-09-07 12:09:12 -0700

    Spruce up demo by hiding map clipping issues

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,8 @@ mod map_data;
 mod wasm4;
 
 use crate::audio::{music, music_update};
-use crate::font::{btext, fmetrics, ftext};
-use crate::gfx::SplitSprite;
+use crate::font::{fmetrics, ftext, Font};
+use crate::gfx::{thick_hline, thick_line, SplitSprite};
 use wasm4::*;
 
 const ANIMATION_CYCLE_LEN: u32 = 2 * 5 * 60;
@@ -89,11 +89,25 @@ fn update() {
         unsafe { (&mut *PALETTE)[i] = c }
     }
 
+    // Cheat past our map clipping problem by drawing the bricks on top of the map.
+    // TODO: map clipping
+    let bg_split_y: u32 = 80;
+    let map_cycle = (animation_clock as i32 % 200) / 10;
+    unsafe { *DRAW_COLORS = 0x1234 }
+    map_data::VILLAGE.draw(
+        0,
+        bg_split_y as i32,
+        character.map_x + map_cycle,
+        character.map_y + map_cycle,
+        SCREEN_SIZE,
+        SCREEN_SIZE - bg_split_y,
+    );
+
     // Loops 15 times in the animation cycle assuming 8x? tile..
     let bg_cycle = (animation_clock / 5) % BG_BRICKS_WIDTH;
     // Intentionally drawing one more column than would fill the screen.
     for x in (0..=SCREEN_SIZE).step_by(BG_BRICKS_WIDTH as usize) {
-        for y in (0..SCREEN_SIZE).step_by(BG_BRICKS_HEIGHT as usize) {
+        for y in (0..bg_split_y).step_by(BG_BRICKS_HEIGHT as usize) {
             blit(
                 &BG_BRICKS,
                 (x - bg_cycle) as i32,
@@ -105,9 +119,6 @@ fn update() {
         }
     }
 
-    unsafe { *DRAW_COLORS = 0x1234 }
-    map_data::VILLAGE.draw(0, 90, character.map_x, character.map_y, 160, 70);
-
     let character_bg_start_x = SCREEN_SIZE - character.sprite.w + 6;
     let character_bg_start_y = SCREEN_SIZE - character.sprite.h - 2;
     unsafe { *DRAW_COLORS = 1 }
@@ -115,15 +126,16 @@ fn update() {
         let x = character_bg_start_x - (y - character_bg_start_y) / 4;
         hline(x as i32, y as i32, SCREEN_SIZE - x);
     }
-    unsafe { *DRAW_COLORS = 2 }
-    let thickness = 2;
-    for t in 0..thickness {
-        let x1 = character_bg_start_x as i32 - t;
-        let y1 = character_bg_start_y as i32 - t;
+    // draw border
+    {
+        unsafe { *DRAW_COLORS = 3 }
+        let thickness = 2;
+        let x1 = character_bg_start_x as i32;
+        let y1 = character_bg_start_y as i32;
         let x2 = x1 - (SCREEN_SIZE as i32 - y1) / 4;
         let y2 = SCREEN_SIZE as i32;
-        hline(x1, y1, SCREEN_SIZE - x1 as u32);
-        line(x1, y1, x2, y2);
+        thick_hline(x1, y1, SCREEN_SIZE - x1 as u32, thickness);
+        thick_line(x1, y1, x2, y2, thickness, thickness);
     }
 
     character.sprite.blit(
@@ -132,14 +144,12 @@ fn update() {
         0,
     );
 
-    unsafe { *DRAW_COLORS = 0x11 }
-    rect(9, 9, 10 * character.name.len() as u32 + 2, 10 + 2);
-    shadow_text(character.name, 10, 10);
+    shadow_text(character.name, 5, 5);
 
     for (i, t) in character.bio.into_iter().enumerate() {
-        let x = 10;
-        let y = 25 + (i as i32) * 15;
-        shadow_btext(b"\x85", x, y - 1);
+        let x = 5;
+        let y = 20 + (i as i32) * 15;
+        shadow_text("➡️", x, y - 1);
         shadow_ftext(t, x + 10, y);
     }
 
@@ -149,17 +159,13 @@ fn update() {
 }
 
 fn shadow_text(t: &str, x: i32, y: i32) {
+    let (mw, mh) = Font::BuiltIn.metrics(t);
+    unsafe { *DRAW_COLORS = 0x11 }
+    rect(x - 1, y - 1, mw + 2, mh + 2);
     unsafe { *DRAW_COLORS = 0x3 }
-    text(t, x + 1, y + 1);
+    Font::BuiltIn.text(t, x + 1, y + 1);
     unsafe { *DRAW_COLORS = 0x4 }
-    text(t, x, y);
-}
-
-fn shadow_btext(t: &[u8], x: i32, y: i32) {
-    unsafe { *DRAW_COLORS = 0x3 }
-    btext(t, x + 1, y + 1);
-    unsafe { *DRAW_COLORS = 0x4 }
-    btext(t, x, y);
+    Font::BuiltIn.text(t, x, y);
 }
 
 fn shadow_ftext(t: &str, x: i32, y: i32) {
