e27f3e05db412021069fc7a07721fcb4a3bfe0fb
Vyr Cossont <VyrCossont@users.noreply.github.com>
2022-09-07 22:03:43 -0700

    Make some village tiles transparent to allow for partial occlusion by roofs

--- asset_originals/Kenney-MonochromeRPG.tsx
+++ asset_originals/Kenney-MonochromeRPG.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="Kenney-MonochromeRPG" tilewidth="16" tileheight="16" tilecount="136" columns="17">
- <image source="../assets/Kenney-MonochromeRPG.png" width="272" height="128"/>
+<tileset version="1.9" tiledversion="1.9.1" name="Kenney-MonochromeRPG" tilewidth="16" tileheight="16" tilecount="152" columns="19">
+ <image source="../assets/Kenney-MonochromeRPG.png" trans="000000" width="304" height="128"/>
 </tileset>
