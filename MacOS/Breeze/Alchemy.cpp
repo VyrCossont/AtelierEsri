fd2f5cc5b04cfd529782eb439b486fa1a8abaeac
Vyr Cossont <VyrCossont@users.noreply.github.com>
2023-12-19 00:58:18 +0100

    WIP: Implement a big chunk of synthesis data model
    TODO: write tests

--- MacOS/Breeze/Alchemy.cpp
+++ MacOS/Breeze/Alchemy.cpp
@@ -4,6 +4,103 @@
 
 namespace Breeze {
 
+std::vector<std::reference_wrapper<const Item>> SynthesisState::AllowedItemsFor(
+    const RecipeNode &node
+) const {
+  std::vector<std::reference_wrapper<const Item>> items{};
+
+  if (const auto materialInput =
+          std::get_if<std::reference_wrapper<Material>>(&node.input)) {
+    const Material &material = *materialInput;
+    for (const Item &item : inventory) {
+      if (&item.material == &material) {
+        items.emplace_back(item);
+      }
+    }
+
+  } else if (const auto categoryInput = std::get_if<Category>(&node.input)) {
+    const Category category = *categoryInput;
+    for (const Item &item : inventory) {
+      if (item.categories.test(category)) {
+        items.emplace_back(item);
+      }
+    }
+
+  } else {
+    assert(false, "Unknown material input type");
+  }
+
+  return items;
+}
+
+void SynthesisState::Place(const RecipeNode &node, const Item &item) {
+  placements.push_back({node, item});
+}
+
+bool SynthesisState::CanFinish() const {
+  // ReSharper disable once CppUseStructuredBinding
+  for (const auto &node : recipe.nodes) {  // NOLINT(*-use-anyofallof)
+    if (node.elementalUnlockRequirement.has_value() ||
+        node.qualityUnlockRequirement.has_value()) {
+      // This node is not mandatory. Ignore it.
+      continue;
+    }
+
+    // This node is mandatory and needs at least one item.
+    if (ItemsPlacedIn(node).empty()) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool SynthesisState::CanPlace() const {
+  return maxPlacements > placements.size();
+}
+
+bool SynthesisState::Unlocked(const RecipeNode &node) const {
+  if (node.elementalUnlockRequirement) {
+    assert(
+        node.parent,
+        "Malformed recipe: "
+        "node has elemental unlock requirement but no parent"
+    );
+    const Element element = node.elementalUnlockRequirement->element;
+    const ElementCount requiredValue = node.elementalUnlockRequirement->count;
+    const RecipeNode &parent = *node.parent;
+    if (ElementValue(parent, element) < requiredValue) {
+      return false;
+    }
+  }
+
+  if (node.qualityUnlockRequirement) {
+    const Quality requiredQuality = *node.qualityUnlockRequirement;
+    if (Result().item.quality < requiredQuality) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+ElementCount SynthesisState::ElementValue(const RecipeNode &node) const {
+  return ElementValue(node, node.element);
+}
+
+ElementCount SynthesisState::ElementValue(
+    const RecipeNode &node, const Element element
+) const {
+  ElementCount elementValue = 0;
+  // ReSharper disable once CppRangeBasedForIncompatibleReference
+  for (const Item &item : ItemsPlacedIn(node)) {
+    if (item.elements.test(element)) {
+      elementValue += item.elementValue;
+    }
+  }
+  return elementValue;
+}
+
 SynthesisResult SynthesisState::Result() const {
   SynthesisResult result = {
       .item =
@@ -13,6 +110,8 @@ SynthesisResult SynthesisState::Result() const {
               .elementValue = recipe.material.elementValue,
               .categories = recipe.material.categories,
               .traits = recipe.material.traits,
+              .effects = {},
+              .slotEffects = {},
           },
       .quantity = 1
   };
@@ -24,20 +123,21 @@ SynthesisResult SynthesisState::Result() const {
   result.item.quality /= static_cast<int>(placements.size());
 
   // For each node, apply any unlocked effects.
-  for (auto &recipeNode : recipe.nodes) {
+  for (auto &node : recipe.nodes) {
     ElementCount elementValue = 0;
-    for (const auto &[node, item] : placements) {
-      if (&recipeNode == &node && item.elements[recipeNode.element]) {
+    for (const auto &[placementNode, item] : placements) {
+      if (&node == &placementNode && item.elements[node.element]) {
         elementValue += result.item.elementValue;
       }
     }
 
-    for (auto &[effect, count] : recipeNode.effects) {
+    for (auto &[effect, slot, count] : node.effects) {
       if (count < elementValue) {
         break;
       }
       elementValue -= count;
 
+      // Some effects are consumed during synthesis.
       // ReSharper disable CppDeclarationHidesLocal
       if (const auto e = std::get_if<EffectQuality>(&effect)) {
         result.item.quality += e->bonus;
@@ -48,14 +148,34 @@ SynthesisResult SynthesisState::Result() const {
       } else if (const auto e = std::get_if<EffectAddCategory>(&effect)) {
         result.item.categories[e->category] = true;
       } else {
-        // TODO: many effects can be passed through to the trait list
-        assert("Unknown variant case for Breeze::Effect");
+        if (slot) {
+          result.item.slotEffects[*slot] = effect;
+        } else {
+          // TODO: implement stacking/merging
+          result.item.effects.push_back(effect);
+        }
       }
       // ReSharper restore CppDeclarationHidesLocal
     }
   }
 
+  // Quality cap.
+  result.item.quality = std::min(maxQuality, result.item.quality);
+
   return result;
+}
+
+// TODO: add lookup structure for this
+std::vector<std::reference_wrapper<const Item>> SynthesisState::ItemsPlacedIn(
+    const RecipeNode &node
+) const {
+  std::vector<std::reference_wrapper<const Item>> items{};
+  for (const auto &[placementNode, item] : placements) {
+    if (&placementNode == &node) {
+      items.emplace_back(item);
+    }
+  }
+  return items;
 }
 
 std::vector<Material> Material::Catalog() {
@@ -82,6 +202,18 @@ std::vector<Material> Material::Catalog() {
   catalog.push_back(material);
 
   return catalog;
+}
+
+std::vector<std::reference_wrapper<const Effect>> Item::AllEffects() const {
+  std::vector<std::reference_wrapper<const Effect>> allEffects{};
+  allEffects.insert(allEffects.end(), traits.begin(), traits.end());
+  allEffects.insert(allEffects.end(), effects.begin(), effects.end());
+  for (const auto &slotEffect : slotEffects) {
+    if (slotEffect) {
+      allEffects.emplace_back(*slotEffect);
+    }
+  }
+  return allEffects;
 }
 
 }  // namespace Breeze
--- MacOS/Breeze/Alchemy.hpp
+++ MacOS/Breeze/Alchemy.hpp
@@ -10,16 +10,16 @@
 #include <vector>
 
 #include "EnumSet.hpp"
+#include "Geometry/V2.hpp"
 
 namespace Breeze {
 
 using Quality = int;
 using ElementCount = int;
+using EffectSlot = int;
+constexpr int NumEffectSlots = 4;
 
-struct RecipeGridPoint {
-  int x;
-  int y;
-};
+struct RecipeGridPoint : V2<RecipeGridPoint, int> {};
 
 // NOLINTBEGIN(*-explicit-constructor, *-no-recursion)
 
@@ -65,18 +65,22 @@ BETTER_ENUM(
 
 // NOLINTEND(*-explicit-constructor, *-no-recursion)
 
+/// Consumed in synthesis.
 struct EffectQuality {
   Quality bonus;
 };
 
+/// Consumed in synthesis.
 struct EffectSynthQuantity {
   int bonus;
 };
 
+/// Consumed in synthesis.
 struct EffectAddElement {
   Element element;
 };
 
+/// Consumed in synthesis.
 struct EffectAddCategory {
   Category category;
 };
@@ -86,12 +90,16 @@ struct EffectElementalDamage {
   int amount;
 };
 
+/// Does not stack with itself.
+struct EffectSingleTarget {};
+
 using Effect = std::variant<
     EffectQuality,
     EffectSynthQuantity,
     EffectAddElement,
     EffectAddCategory,
-    EffectElementalDamage>;
+    EffectElementalDamage,
+    EffectSingleTarget>;
 
 /// An abstract material, such as red neutralizer.
 struct Material {
@@ -117,9 +125,19 @@ struct Item {
   Quality quality;
   /// Should be a superset of `material.categories`.
   EnumSet<Category> categories;
-  // TODO: effects (1, 2, 3, 4) and traits (from materials) should be separated.
   /// Should be a superset of `material.traits`.
+  /// Traits are transferrable.
   std::vector<Effect> traits;
+  /// Effects are added during synthesis and aren't transferrable.
+  std::vector<Effect> effects;
+  /// Some effects apply to item effect slots.
+  /// These can only be transferred through chain synthesis,
+  /// and are replaced by other effects that target the same slot.
+  std::array<std::optional<Effect>, NumEffectSlots> slotEffects;
+
+  /// Combined list of all types of effect.
+  [[nodiscard]] std::vector<std::reference_wrapper<const Effect>> AllEffects(
+  ) const;
 };
 
 /// Recipe nodes can take either a specific material or a category.
@@ -135,20 +153,23 @@ struct RecipeNodeElementalRequirement {
 
 struct RecipeNodeEffect {
   Effect effect;
+  std::optional<EffectSlot> slot;
   ElementCount count;
 };
 
 /// Node in an abstract recipe.
+///
+/// Nodes that do not have an elemental or quality unlock requirement are
+/// considered mandatory by the recipe, and must have items placed in them to
+/// finish the synthesis.
 struct RecipeNode {
   RecipeGridPoint gridPos;
   Element element;
   RecipeNodeInput input;
   std::vector<RecipeNodeEffect> effects;
-  std::optional<RecipeNodeElementalRequirement> elementalRequirement;
-  std::optional<Quality> qualityRequirement;
-  /// All required nodes must have at least one item to finish synthesis.
-  bool required;
-  std::optional<std::reference_wrapper<RecipeNode>> parent;
+  std::optional<RecipeNodeElementalRequirement> elementalUnlockRequirement;
+  std::optional<Quality> qualityUnlockRequirement;
+  std::optional<std::reference_wrapper<const RecipeNode>> parent;
 };
 
 struct Recipe {
@@ -158,8 +179,8 @@ struct Recipe {
 
 /// Item placed at a node. Mostly useful as an undo stack entry.
 struct SynthesisPlacement {
-  RecipeNode &node;
-  Item &item;
+  const RecipeNode &node;
+  const Item &item;
 };
 
 struct SynthesisResult {
@@ -167,11 +188,30 @@ struct SynthesisResult {
   int quantity{};
 };
 
+using PlayerInventory = std::vector<Item>;
+
 struct SynthesisState {
   Recipe &recipe;
+
   /// Determined by your alchemy level and skills.
   int maxPlacements;
+  /// Determined by your alchemy level and skills.
+  int maxQuality;
+
+  /// Input: your inventory.
+  PlayerInventory inventory;
+
+  /// Items you've placed on nodes.
   std::vector<SynthesisPlacement> placements;
+
+  /// Items allowed for a given node.
+  /// Skips items you've already used.
+  [[nodiscard]] std::vector<std::reference_wrapper<const Item>> AllowedItemsFor(
+      const RecipeNode &node
+  ) const;
+
+  /// Add an item to a node.
+  void Place(const RecipeNode &node, const Item &item);
 
   /// Can we finish the synthesis?
   [[nodiscard]] bool CanFinish() const;
@@ -179,11 +219,25 @@ struct SynthesisState {
   /// Can we add any more items?
   [[nodiscard]] bool CanPlace() const;
 
+  /// Are this node's unlock requirements fulfilled?
+  [[nodiscard]] bool Unlocked(const RecipeNode &node) const;
+
+  /// Element value of items placed on a node matching the node's element.
+  [[nodiscard]] ElementCount ElementValue(const RecipeNode &node) const;
+
+  /// Element value of items placed on a node matching an arbitrary element.
+  [[nodiscard]] ElementCount ElementValue(
+      const RecipeNode &node, Element element
+  ) const;
+
+  /// Items placed in a given node.
+  [[nodiscard]] std::vector<std::reference_wrapper<const Item>> ItemsPlacedIn(
+      const RecipeNode &node
+  ) const;
+
   /// Return the current state of the item being synthesized,
   /// including *all* available traits.
   [[nodiscard]] SynthesisResult Result() const;
 };
-
-using PlayerInventory = std::vector<Item>;
 
 }  // namespace Breeze
