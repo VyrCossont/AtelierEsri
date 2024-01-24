d74847b2a8b322d8ba023ccc5c45fd40485a16e1
Vyr Cossont <VyrCossont@users.noreply.github.com>
2024-01-23 19:35:26 -0800

    Extend KMRPG tileset, start transcribing atelier exterior map

--- asset_originals/tilesets/Kenney_monochromerpg_extended.tsx
+++ asset_originals/tilesets/Kenney_monochromerpg_extended.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="Kenney-MonochromeRPG" tilewidth="16" tileheight="16" tilecount="152" columns="19">
- <image source="Kenney_monochromerpg_extended.png" trans="000000" width="304" height="128"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Kenney-MonochromeRPG" tilewidth="16" tileheight="16" tilecount="216" columns="27">
+ <image source="Kenney_monochromerpg_extended.png" width="432" height="128"/>
 </tileset>
