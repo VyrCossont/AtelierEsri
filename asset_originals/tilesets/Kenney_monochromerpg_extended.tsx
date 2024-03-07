dbaa66088288d613fa089ae7ab9b92285ac03e4a
Vyr Cossont <VyrCossont@users.noreply.github.com>
2024-03-07 09:36:38 -0800

    Add map and some tile collision data

--- asset_originals/tilesets/Kenney_monochromerpg_extended.tsx
+++ asset_originals/tilesets/Kenney_monochromerpg_extended.tsx
@@ -1,4 +1,50 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Kenney-MonochromeRPG" tilewidth="16" tileheight="16" tilecount="216" columns="27">
  <image source="Kenney_monochromerpg_extended.png" width="432" height="128"/>
+ <tile id="55">
+  <objectgroup draworder="index" id="3">
+   <object id="7" x="2" y="0">
+    <polygon points="0,0 -2,0 -2,12 2,16 14,16 14,4 3,4 0,2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="14" y="0">
+    <polygon points="0,0 0,2 -3,4 -14,4 -14,16 -2,16 2,12 2,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="16" y="1">
+    <polygon points="0,0 -6,0 -10,1 -14,4 -15,9 -15,15 -2,15 -2,13 0,13"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="142">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="143">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="144">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="145">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
