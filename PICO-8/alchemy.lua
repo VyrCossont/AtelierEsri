bc90c86689d243f670a9a2a892fed2e98b756fa2
Vyr Cossont <vyr@noreply.codeberg.org>
2025-07-22 21:44:18 -0700

    draw transmutation diagram

---
+++ PICO-8/alchemy.lua
@@ -0,0 +1,275 @@
+-- alchemy data model
+
+function match_exact(material_id)
+    return function(item)
+        return item.material.id == material_id
+    end
+end
+
+function match_type(material_type)
+    return function(item)
+        return item.material.type == material_type
+    end
+end
+
+-- effects
+
+function set_item(id)
+    return function(item)
+        item.id = id
+    end
+end
+
+-- material types
+material_type_ingot = 'ingot'
+material_type_ore = 'ore'
+
+-- blitz ore material
+blitz_ore = {
+    id = 'blitz_ore',
+}
+
+-- copper ingot material
+copper_ingot = {
+    id = 'copper_ingot',
+    nodes = nil, -- TODO
+}
+
+-- slot IDs
+ring_1_c  = 1
+
+ring_2_e  = 1
+ring_2_w  = 2
+
+ring_3_ne = 1
+ring_3_nw = 2
+ring_3_sw = 3
+ring_3_se = 4
+
+ring_4_ne = 1
+ring_4_n  = 2
+ring_4_nw = 3
+ring_4_sw = 4
+ring_4_s  = 5
+ring_4_se = 6
+
+-- each pair of rings maps a slot in the first ring
+-- to a list of slots in the second ring
+ring_links = {
+    -- ring 1 to 2
+    {
+        {
+            ring_1_c,
+            {
+                ring_2_e,
+            },
+        },
+        {
+            ring_1_c,
+            {
+                ring_2_w,
+            },
+        },
+    },
+    -- ring 2 to 3
+    {
+        {
+            ring_2_e,
+            {
+                ring_3_ne,
+                ring_3_se,
+            },
+        },
+        {
+            ring_2_w,
+            {
+                ring_3_nw,
+                ring_3_sw,
+            },
+        },
+    },
+    -- ring 3 to 4
+    {
+        {
+            ring_3_ne,
+            {
+                ring_4_ne,
+                ring_4_n,
+            },
+        },
+        {
+            ring_3_nw,
+            {
+                ring_4_n,
+                ring_4_nw,
+            },
+        },
+        {
+            ring_3_sw,
+            {
+                ring_4_sw,
+                ring_4_s,
+            },
+        },
+        {
+            ring_3_se,
+            {
+                ring_4_s,
+                ring_4_se,
+            },
+        }
+    },
+}
+
+-- static pylon recipe
+pylon = {
+    id = 'pylon',
+    nodes = {
+        -- required
+        {
+            {
+                condition = match_exact(copper_ingot.id),
+                -- ring 1 has the implicit effect of setting the item id
+            }
+        },
+        -- major variant
+        {
+            -- range
+            {
+                condition = match_type(material_type_ingot),
+            },
+            -- damage
+            {
+                condition = match_exact(blitz_ore.id),
+                effect
+            },
+        },
+        -- minor variant or bonus
+        --
+    },
+}
+
+function lerp(a, b, t)
+    return (1 - t) * a + t * b
+end
+
+-- slow but precise filled arc
+-- assumes rmin < rmax and thetamin < thetamax
+function arcfill(cx, cy, rmin, rmax, thetamin, thetamax, thetaoffset, color)
+    local xmin = cx - rmax
+    local xmax = cx + rmax
+    local ymin = cy - rmax
+    local ymax = cy + rmax
+    for y = ymin, ymax do
+        for x = xmin, xmax do
+            local dx = x - cx
+            local dy = y - cy
+            local r = sqrt(dx * dx + dy * dy)
+            local theta = atan2(dx, dy) - thetaoffset
+            theta = theta % 1
+            if rmin <= r and r < rmax and thetamin <= theta and theta < thetamax then
+                pset(x, y, color)
+            end
+        end
+    end
+end
+
+-- return center (x, y) of arc
+function arccenter(cx, cy, rmin, rmax, thetamin, thetamax, thetaoffset)
+    local r = lerp(rmin, rmax, 0.5)
+    local theta = lerp(thetamin, thetamax, 0.5) + thetaoffset
+    theta = theta % 1
+    return cx + r * cos(theta), cy + r * sin(theta)
+end
+
+function draw_alchemy_diagram()
+    local cx = 64
+    local cy = 64
+    local r = 16
+    local color = 0
+    local ring_max = 4
+
+    local sector_centers = {}
+
+    -- draw ring 1 (the required center node)
+    circfill(cx, cy, r / 2, color)
+    color = color + 1
+    sector_centers[1] = {{cx, cy}}
+
+    -- draw rings 2-4
+    for ring_index = 2, ring_max do
+        sector_centers[ring_index] = {}
+        local num_slots = 2 * (ring_index - 1)
+        local dtheta = 1 / num_slots
+        local rmin = (r / 2) + r * (ring_index - 2)
+        local rmax = rmin + r
+        local thetaoffset = 0
+        -- ring 2 is rotated halfway to look nicer
+        if ring_index == 2 then
+            thetaoffset = thetaoffset - 0.25
+        end
+        for sector_index = 1, num_slots do
+            local thetamin = (sector_index - 1) * dtheta
+            local thetamax = thetamin + dtheta
+            color = color + 1
+            arcfill(
+                    cx,
+                    cy,
+                    rmin,
+                    rmax,
+                    thetamin,
+                    thetamax,
+                    thetaoffset,
+                    color
+            )
+            local tx, ty = arccenter(
+                    cx,
+                    cy,
+                    rmin,
+                    rmax,
+                    thetamin,
+                    thetamax,
+                    thetaoffset
+            )
+            sector_centers[ring_index][sector_index] = {tx, ty}
+        end
+    end
+
+    -- draw inter-node lines and collect nodes
+    -- todo: light up only paths that can be selected
+    -- start with the root node
+    local node_centers = {{{cx, cy}}}
+    for ring_index_2 = 2, ring_max do
+        node_centers[ring_index_2] = {}
+        local ring_index_1 = ring_index_2 - 1
+        for sector_links in all(ring_links[ring_index_1]) do
+            local sector_index_1, sector_indexes = unpack(sector_links)
+            for sector_index_2 in all(sector_indexes) do
+                local x1, y1 = unpack(sector_centers[ring_index_1][sector_index_1])
+                local x2, y2 = unpack(sector_centers[ring_index_2][sector_index_2])
+                -- draw lines between ring centers
+                line(x1, y1, x2, y2, 0)
+                -- add the node at the end of the line
+                node_centers[ring_index_2][sector_index_2] = {x2, y2}
+            end
+        end
+    end
+
+    -- draw nodes
+    -- todo: light up only filled nodes
+    local shape = ord('A') + 65
+    for ring_nodes in all(node_centers) do
+        for sector_nodes in all(ring_nodes) do
+            local x, y = unpack(sector_nodes)
+            circfill(x, y, r * 0.35, 0)
+            print(chr(shape), x - 3, y - 2, 7)
+            shape = shape + 1
+        end
+    end
+end
+
+function _draw()
+    cls()
+
+    draw_alchemy_diagram()
+end
---
+++ PICO-8/atelier_esri.p8
@@ -0,0 +1,8 @@
+pico-8 cartridge // http://www.pico-8.com
+version 42
+__lua__
+-- Geometry Wars x Atelier
+-- @vyr@princess.industries
+
+#include pinput.lua
+#include alchemy.lua
